422f331a964313a7f9a9074f4a0b39ff10cb09c4
mucrow <120820315+mucrow@users.noreply.github.com>
2023-02-13 16:06:21 -0500

    create git repository

---
+++ UntitledPlatformerFeb2023/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Upf23 {
+  [RequireComponent(typeof(Rigidbody2D))]
+  public class Player: MonoBehaviour {
+    [SerializeField] float _moveSpeed = 10f;
+    [SerializeField] float _jumpForce = 400f;
+
+    Rigidbody2D _rigidbody2D;
+
+    float _moveInput = 0;
+
+    void Awake() {
+      _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+      Vector2 velocity = _rigidbody2D.velocity;
+      velocity.x = _moveInput * _moveSpeed;
+      _rigidbody2D.velocity = velocity;
+    }
+
+    public void OnFire(InputValue value) => _rigidbody2D.AddForce(new Vector2(0, _jumpForce));
+    public void OnMove(InputValue value) => _moveInput = value.Get<Vector2>().x;
+  }
+}
